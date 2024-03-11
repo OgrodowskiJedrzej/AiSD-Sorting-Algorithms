48118b6d800dce3755720647566dc0d654b5044b
OgrodowskiJedrzej <jedrekogro@gmail.com>
2024-03-11 23:58:40 +0100

    Remove extra code, bug fixes, improved code consistency

--- source/main.cpp
+++ source/main.cpp
@@ -4,8 +4,6 @@
 
 void insertion_sort(int *data, int size)
 {
-    std::cout << "insertion_sort in use"
-              << "\n";
     int key, j;
     for (int i = 1; i < size; i++)
     {
@@ -20,10 +18,48 @@ void insertion_sort(int *data, int size)
     }
 }
 
+long sedgewick_expression(int k)
+{
+    return std::pow(4, k + 1) + 3 * std::pow(2, k) + 1;
+}
+
+std::vector<long> generate_sedgewick_gaps(int k)
+{
+    std::vector<long> gaps(20);
+    for (int i = 0; i < 20; i++)
+    {
+        gaps[i] = sedgewick_expression(i);
+    }
+    return gaps;
+}
+// TODO fix shell sort
+void shellSort(int *data, int size, const std::vector<long> &sedgewick_gaps)
+{
+    int gap_index = 0;
+
+    while (sedgewick_gaps[gap_index] < size / 3)
+    {
+        gap_index++;
+    }
+
+    for (int gap = sedgewick_gaps[gap_index]; gap > 0; gap = sedgewick_gaps[--gap_index])
+    {
+        for (int i = gap; i < size; i += 1)
+        {
+            int temp = data[i];
+            int j;
+
+            for (j = i; j >= gap && data[j - gap] > temp; j -= gap)
+            {
+                data[j] = data[j - gap];
+            }
+            data[j] = temp;
+        }
+    }
+}
+
 void selection_sort(int *data, int size)
 {
-    std::cout << "selection_sort in use"
-              << "\n";
     int min_idx;
     for (int i = 0; i < size - 1; i++)
     {
@@ -31,10 +67,14 @@ void selection_sort(int *data, int size)
         for (int j = i + 1; j < size; j++)
         {
             if (data[j] < data[min_idx])
+            {
                 min_idx = j;
+            }
         }
         if (min_idx != i)
+        {
             std::swap(data[min_idx], data[i]);
+        }
     }
 }
 
@@ -70,42 +110,10 @@ void quick_sort_left_pivot(int *data, int low, int high)
     }
 }
 
-int partition_right(int *data, int low, int high)
-{
-    int pivot = data[high];
-    int p = low;
-    int q = high - 1;
-
-    while (true)
-    {
-        while (p <= q && data[p] <= pivot)
-            p++;
-        while (p <= q && data[q] >= pivot)
-            q--;
-        if (p <= q)
-            std::swap(data[p], data[q]);
-        else
-            break;
-    }
-
-    std::swap(data[high], data[p]);
-    return p;
-}
-
-void quick_sort_right_pivot(int *data, int low, int high)
-{
-    if (low < high)
-    {
-        int pivot = partition_right(data, low, high);
-        quick_sort_right_pivot(data, low, pivot - 1);
-        quick_sort_right_pivot(data, pivot + 1, high);
-    }
-}
-
 int partition_random(int *data, int low, int high)
 {
-    int randIndex = rand()%(high-low+1)+low;
-    std::swap(data[high], data[randIndex]);
+    int rand_index = rand() % (high - low + 1) + low;
+    std::swap(data[high], data[rand_index]);
     int pivot = data[high];
     int p = low;
     int q = high - 1;
@@ -113,13 +121,21 @@ int partition_random(int *data, int low, int high)
     while (true)
     {
         while (p <= q && data[p] <= pivot)
+        {
             p++;
+        }
         while (p <= q && data[q] >= pivot)
+        {
             q--;
+        }
         if (p <= q)
+        {
             std::swap(data[p], data[q]);
+        }
         else
+        {
             break;
+        }
     }
 
     std::swap(data[high], data[p]);
@@ -193,8 +209,6 @@ void shift_down(int *data, int i, int upper)
 
 void heap_sort(int *data, int size)
 {
-    std::cout << "heap sort in use"
-              << "\n";
     for (int j = (size - 2) / 2; j > -1; j--)
     {
         shift_down(data, j, size);
@@ -238,35 +252,49 @@ int main(int argc, char *argv[])
     switch (algorithm_number)
     {
     case 1:
+    {
         insertion_sort(data, count);
         break;
+    }
     case 2:
-        // TODO shell sort
+    {
+        std::vector<long> sedgewick_gaps = generate_sedgewick_gaps(20);
+        shellSort(data, count, sedgewick_gaps);
         break;
+    }
     case 3:
+    {
         selection_sort(data, count);
         break;
+    }
     case 4:
+    {
         heap_sort(data, count);
         break;
+    }
     case 5:
-        // TODO quick_sort_left_pivot
+    {
+        quick_sort_left_pivot(data, 0, count - 1);
         break;
+    }
     case 6:
-        // TODO quick_sort_random_pivot
+    {
+        quick_sort_random_pivot(data, 0, count - 1);
         break;
+    }
     default:
+    {
         std::cerr << "Wrong algorithm number"
                   << "\n";
     }
 
-    // Print the sorted data
-    std::cout << "Sorted data: ";
-    for (int i = 0; i < std::min(10, count); ++i)
-    {
-        std::cout << data[i] << " ";
-    }
-    std::cout << std::endl;
+        // Print the sorted data
+        std::cout << "Sorted data: ";
+        for (int i = 0; i < std::min(10, count); ++i)
+        {
+            std::cout << data[i] << " ";
+        }
+        std::cout << "\n";
 
-    return 0;
-}
+        return 0;
+    }
